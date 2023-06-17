70d52be53f89fabf5515f66b6bbf72056ad24d82
jonas breen <jonasebreen@gmail.com>
2023-06-16 21:56:54 -0400

    added rust install to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
+# install curl
+sudo apt install curl -y
+
+# install rust and cargo
+curl https://sh.rustup.rs -sSf | sh
+
 git clone git@github.com:propbreakerfpv/dotfilers.git
 cd dotfilers
 cargo install --path .
